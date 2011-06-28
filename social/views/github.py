84085338884883a5112640a5e03a13872d857ffc
Craig Slusher <cslush@gmail.com>
2011-06-27 21:01:15 -0700

    updated to use a real oauth2 library

--- social/views/github.py
+++ social/views/github.py
@@ -1,29 +1,21 @@
-import urllib
-import urllib2
-import urlparse
+import oauth2
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 
+BASE_URL = 'https://github.com/login/oauth/'
 CLIENT_ID = getattr(settings, 'GITHUB_CLIENT_ID')
 CLIENT_SECRET = getattr(settings, 'GITHUB_CLIENT_SECRET')
 
-AUTHORIZE_URL = 'https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID
-ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
-
 def github_login(request):
-    return HttpResponseRedirect(AUTHORIZE_URL)
+    client = oauth2.Client2(CLIENT_ID, CLIENT_SECRET, BASE_URL)
+    authorization_url = client.authorization_url()
+    return HttpResponseRedirect(authorization_url)
 
 def github_login_done(request):
-    parameters = {
-            'code': request.GET['code'],
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET
-            }
-
-    result = urllib2.urlopen(ACCESS_TOKEN_URL, urllib.urlencode(parameters))
-    parsed_qs = urlparse.parse_qs(result.read())
-    access_token = parsed_qs['access_token'][0]
+    client = oauth2.Client2(CLIENT_ID, CLIENT_SECRET, BASE_URL)
+    data = client.access_token(request.GET['code'])
+    access_token = data.get('access_token')
 
     user = authenticate(access_token=access_token)
 
