3359436dafdd9275f141eccaa64e5f1978884a0e
Craig Slusher <cslush@gmail.com>
2011-08-02 14:57:32 -0700

    class-based generic oauth2 views

--- social/urls.py
+++ social/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
+from social.views.facebook import FacebookCallbackView, FacebookLoginView
+from social.views.github import GithubCallbackView, GithubLoginView
 
 urlpatterns = patterns('social.views',
-    url(r'^facebook-login/$', 'facebook.facebook_login', name='facebook-login'),
-    url(r'^facebook-login/done/$', 'facebook.facebook_login_done', name='facebook-login-done'),
-    url(r'^github-login/$', 'github.github_login', name='github-login'),
-    url(r'^github-login/done/$', 'github.github_login_done', name='github-login-done'),
+    url(r'^facebook-login/$', FacebookLoginView.as_view(), name='facebook-login'),
+    url(r'^facebook-login-done/$', FacebookCallbackView.as_view(), name='facebook-login-done'),
+    url(r'^github-login/$', GithubLoginView.as_view(), name='github-login'),
+    url(r'^github-login/done/$', GithubCallbackView.as_view(), name='github-login-done'),
     url(r'^twitter-login/$', 'twitter.twitter_login', name='twitter-login'),
     url(r'^twitter-login/done/$', 'twitter.twitter_login_done', name='twitter-login-done'),
 )
--- social/views/facebook.py
+++ social/views/facebook.py
@@ -1,34 +1,15 @@
-import oauth2
 from django.conf import settings
-from django.contrib.auth import authenticate, login
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from social.views import OAuth2View, OAuth2LoginMixin, OAuth2CallbackMixin
 
-FACEBOOK_APP_ID = getattr(settings, 'FACEBOOK_APP_ID')
-FACEBOOK_APP_SECRET = getattr(settings, 'FACEBOOK_APP_SECRET')
-BASE_URL = 'https://graph.facebook.com/oauth/'
+class FacebookOAuthView(OAuth2View):
+    app_id = getattr(settings, 'FACEBOOK_APP_ID')
+    app_secret = getattr(settings, 'FACEBOOK_APP_SECRET')
+    base_url = 'https://graph.facebook.com/oauth/'
+    callback_name = 'social:facebook-login-done'
+    authenticate_arg = 'fb_access_token'
 
-def facebook_login(request):
-    client = oauth2.Client2(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, BASE_URL)
+class FacebookLoginView(OAuth2LoginMixin, FacebookOAuthView):
+    pass
 
-    authorization_url = client.authorization_url(
-            redirect_uri=request.build_absolute_uri(reverse('social:facebook-login-done')))
-
-    return HttpResponseRedirect(authorization_url)
-
-def facebook_login_done(request):
-    client = oauth2.Client2(FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, BASE_URL)
-
-    data = client.access_token(
-            request.GET['code'],
-            request.build_absolute_uri(reverse('social:facebook-login-done')))
-
-    user = authenticate(fb_access_token=data.get('access_token'))
-
-    if not user:
-        login_url = getattr(settings, 'LOGIN_URL', '/')
-        return HttpResponseRedirect(login_url)
-
-    login(request, user)
-
-    return HttpResponseRedirect(request.GET.get('next', '/'))
+class FacebookCallbackView(OAuth2CallbackMixin, FacebookOAuthView):
+    pass
