9c6690a5b7d93f774b81d798e1fa50158208d908
Craig Slusher <cslush@gmail.com>
2011-08-10 21:45:54 -0700

    updated to use getattr properly

--- social/views/facebook.py
+++ social/views/facebook.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from social.views import OAuth2View, OAuth2LoginMixin, OAuth2CallbackMixin
 
 class FacebookOAuthView(OAuth2View):
-    app_id = getattr(settings, 'FACEBOOK_APP_ID')
-    app_secret = getattr(settings, 'FACEBOOK_APP_SECRET')
+    app_id = getattr(settings, 'FACEBOOK_APP_ID', None)
+    app_secret = getattr(settings, 'FACEBOOK_APP_SECRET', None)
     base_url = 'https://graph.facebook.com/oauth/'
     callback_name = 'social:facebook-login-done'
     authenticate_arg = 'fb_access_token'
