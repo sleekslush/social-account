57c0a979d03dfbc9835f61f76c230ae3fbf0708d
Craig Slusher <cslush@gmail.com>
2011-08-23 00:07:17 -0700

    added ?next support and fixed an authenticate bug

--- social/views/twitter.py
+++ social/views/twitter.py
@@ -2,12 +2,14 @@ from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
 from social.networks.oauthtwitter import OAuthApi
+from social.views import get_next_url, delete_next_url, reset_next_url
 
 CONSUMER_KEY = getattr(settings, 'TWITTER_CONSUMER_KEY')
 CONSUMER_SECRET = getattr(settings, 'TWITTER_CONSUMER_SECRET')
 REQUEST_TOKEN_SESSION_ID = 'twitter:request_token'
 
 def twitter_login(request):
+    reset_next_url(request)
     twitter = OAuthApi(CONSUMER_KEY, CONSUMER_SECRET)
     request_token = twitter.getRequestToken()
     request.session[REQUEST_TOKEN_SESSION_ID] = request_token
@@ -19,7 +21,7 @@ def twitter_login_done(request):
     request_token = request.session[REQUEST_TOKEN_SESSION_ID]
     access_token = twitter.getAccessToken(request_token)
 
-    user = authenticate(access_token=access_token)
+    user = authenticate(twitter_access_token=access_token)
 
     if not user:
         login_url = getattr(settings, 'LOGIN_URL', '/')
@@ -27,4 +29,6 @@ def twitter_login_done(request):
 
     login(request, user)
 
-    return HttpResponseRedirect(request.GET.get('next', '/'))
+    next_url = get_next_url(request.session)
+
+    return HttpResponseRedirect(next_url)
